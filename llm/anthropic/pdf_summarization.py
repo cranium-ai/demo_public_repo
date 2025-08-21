3c055e1a623040e9e7937304cebc6f9c0f55a8ec
Mike Tan <mtan@cranium.ai>
2025-08-21 14:24:35 -0700

    simplified anthropic api example

--- llm/anthropic/pdf_summarization.py
+++ llm/anthropic/pdf_summarization.py
@@ -1,42 +1,8 @@
-import base64
-from anthropic import Anthropic
+from anthropic import AsyncAnthropic
 
-def get_completion(client, messages):
-    return client.messages.create(
-        model="claude-3-5-sonnet-20241022",
-        max_tokens=2048,
-        messages=messages
-    ).content[0].text
+client = AsyncAnthropic(api_key="api_key")
 
-# While PDF support is in beta, you must pass in the correct beta header
-client = Anthropic(default_headers={
-    "anthropic-beta": "pdfs-2024-09-25"
-  }
+response = client.messages.create(
+    model="claude-3-5-sonnet-20241022",
+    messages=[{"role": "user", "content": "Summarize the following PDF: "}],
 )
-
-# Start by reading in the PDF and encoding it as base64
-file_name = "../multimodal/documents/constitutional-ai-paper.pdf"
-with open(file_name, "rb") as pdf_file:
-  binary_data = pdf_file.read()
-  base64_encoded_data = base64.standard_b64encode(binary_data)
-  base64_string = base64_encoded_data.decode("utf-8")
-
-
-prompt = """
-Please do the following:
-1. Summarize the abstract at a kindergarten reading level. (In <kindergarten_abstract> tags.)
-2. Write the Methods section as a recipe from the Moosewood Cookbook. (In <moosewood_methods> tags.)
-3. Compose a short poem epistolizing the results in the style of Homer. (In <homer_results> tags.)
-"""
-messages = [
-    {
-        "role": 'user',
-        "content": [
-            {"type": "document", "source": {"type": "base64", "media_type": "application/pdf", "data": base64_string}},
-            {"type": "text", "text": prompt}
-        ]
-    }
-]
-
-completion = get_completion(client, messages)
-print(completion)
